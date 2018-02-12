77c1e949c67023139da7ffe132fead59be84e835
Ben Inada <me@beninada.com>
2018-02-11 23:47:43 -0800

    Cleanup routes.

--- routes/users.js
+++ routes/users.js
@@ -7,15 +7,8 @@ var passport = require('passport');
 var auth = require('./auth');
 
 /**
- * GET users listing
+ * GET logged in user
  */
-// router.get('/', function(req, res, next) {
-// 	User.find({}, function(err, users) {
-// 	  if (err) throw err;
-// 		res.json(users);
-// 	});
-// });
-
 router.get('/', auth.required, function(req, res, next) {
   User.findById(req.payload.id).then(function(user) {
     if (!user) { return res.sendStatus(401); }
@@ -43,27 +36,9 @@ router.post('/', function(req, res, next) {
 	}).catch(next);
 });
 
-router.post('/login', function(req, res, next) {
-  if(!req.body.user.email) {
-    return res.status(422).json({ errors: { email: 'cannot be blank' } });
-  }
-
-  if(!req.body.user.password){
-    return res.status(422).json({errors: { password: 'cannot be blank' } });
-  }
-
-  passport.authenticate('local', { session: false }, function(err, user, info) {
-    if (err) { return next(err); }
-
-    if (user) {
-      user.token = user.generateJWT();
-      return res.json({ user: user.toAuthJSON() });
-    } else {
-      return res.status(422).json(info);
-    }
-  })(req, res, next);
-});
-
+/**
+ * PUT a user
+ */
 router.put('/', auth.required, function(req, res, next){
   User.findById(req.payload.id).then(function(user) {
     if (!user) { return res.sendStatus(401); }
@@ -92,6 +67,30 @@ router.put('/', auth.required, function(req, res, next){
       return res.json({ user: user.toAuthJSON() });
     });
   }).catch(next);
+});
+
+/**
+ * POST login a user
+ */
+router.post('/login', function(req, res, next) {
+  if (!req.body.user.email) {
+    return res.status(422).json({ errors: { email: 'cannot be blank' } });
+  }
+
+  if (!req.body.user.password) {
+    return res.status(422).json({errors: { password: 'cannot be blank' } });
+  }
+
+  passport.authenticate('local', { session: false }, function(err, user, info) {
+    if (err) { return next(err); }
+
+    if (user) {
+      user.token = user.generateJWT();
+      return res.json({ user: user.toAuthJSON() });
+    } else {
+      return res.status(422).json(info);
+    }
+  })(req, res, next);
 });
 
 module.exports = router;
